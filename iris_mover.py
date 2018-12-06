cf70a39ecf1dc2d33b94ae09f80becd0d9fc13d3
ajdevaux <32491625+ajdevaux@users.noreply.github.com>
2018-12-06 17:41:47 -0500

    script to combine image files from one experiment

---
+++ iris_mover.py
@@ -0,0 +1,244 @@
+import os, glob, sys, re
+from modules.vpipes import tiff_maker
+# import numpy as np
+# from modules.vpipes import chip_file_reader, sample_namer, zipper
+# from skimage import io as skio
+
+# def iris_renamer(filelist, number):
+#     number = str(number)
+#     # digits = len(number)
+#     files_to_rename = [file for file in filelist if len(file.split(".")) > 3]
+#     mirror_file = str(glob.glob('*000.pgm')).strip("'[]'")
+#     if mirror_file:
+#         files_to_rename.remove(mirror_file)
+#     print(files_to_rename)
+#     toggle = input("OK to rename " + str(len(files_to_rename)) + " files? (y/[n])\t")
+#     if toggle not in ("n", "no"):
+#
+#         for file in files_to_rename:
+#             parsed_file = file.split(".")
+#             if parsed_file[2].isdigit():
+#
+#                 parsed_file[2] = ('0' * (3 - len(number))) + number
+#                 renamed_file = ".".join(parsed_file)
+#                 os.rename(file, renamed_file)
+
+def fluor_cleaner(pgm_list, splitter ='/', fluor_clean_toggle = 'no'):
+    bad_fluor_files = [file for file in pgm_list if file.split(".")[-2] in ['Aold', 'Cold']]
+    if bad_fluor_files:
+        print("\nFluorescent channel(s) detected: {}\n.".format(bad_fluor_files))
+        fluor_clean_toggle = input("Remove fluorescent files? (y/[n])")
+        if fluor_clean_toggle in ('yes', 'y'):
+            for file in bad_fluor_files:
+                os.remove(file)
+                pgm_list.remove(file)
+                print("Deleted {}\n".format(file))
+
+    old_fluor_files = list(filter(re.compile(".*old").match, pgm_list))
+    if old_fluor_files:
+        olddir = 'old'
+        if not os.path.exists(olddir): os.mkdir(olddir)
+        print("Moving old fluorescent files")
+        for file in old_fluor_files:
+            dest = '{}{}{}'.format(olddir, splitter, file)
+            os.rename(file, dest)
+            pgm_list.remove(file)
+
+    return pgm_list
+
+def nV_deleted(file_list, delete_toggle = 'no'):
+    if file_list:
+        print("\nNanoanalysis files detected: {}\n".format(file_list))
+        delete_toggle = input("Remove Nanoanalysis files? (y/[n])")
+        if delete_toggle in ('yes','y'):
+            delete_list = []
+            for file in file_list:
+                print(file)
+                delete_list.append(file)
+                # csv_list.remove(file)
+            print(delete_list)
+            for file in delete_list:
+                os.remove(file)
+                print("Deleted {}\n".format(file))
+
+#***********************************************************************************************#
+def txt_combiner(txt_list, splitter):
+    # if sys.platform == 'win32': splitter = ('\\')
+    # elif sys.platform == 'darwin': splitter = ('/')
+
+    new_txts = ['..' +splitter + file for file in txt_list if len(file.split('.')) == 3]
+    old_txts = [file for file in sorted(glob.glob('*.txt')) if len(file.split('.')) == 3]
+
+    for newfile in new_txts:
+        for oldfile in old_txts:
+            if newfile.split(splitter)[1] == oldfile:
+                print(newfile, '\n', oldfile)
+                of_open = open(oldfile)
+                oldlines = of_open.read()
+                of_open.close()
+
+                oldlines_list = oldlines.split("\n")
+                oldlines_list.remove('')
+                for line in oldlines_list:
+                    if line.startswith('experiment'):
+                        print(line)
+                        expt_start1 = oldlines_list.pop(oldlines_list.index(line))
+                        expt_start1 = expt_start1.split(' ')
+                        start1_date = expt_start1[0].split(':')[1]
+                        start1_timecode = expt_start1[1]
+                        start1_hms = start1_timecode.split(':')
+                        start1_sec = (int(start1_hms[0])*(60**2) + int(start1_hms[1])*60 + int(start1_hms[2]))
+                    # else:
+                    #     print('Cannot find experiment start time')
+
+                old_dict = {k:v for k,v in (val.split(':') for val in oldlines_list)}
+
+                last_pass = int([key[-1] for key in old_dict.keys() if 'pass_time' in key][-1])
+                combo_dict = old_dict.copy()
+    #-----------------------------------------------------------------------------------------------#
+                nf_open = open(newfile)
+                newlines = nf_open.read()
+                nf_open.close()
+
+                newlines_list = newlines.split("\n")
+                newlines_list.remove('')
+                for line in newlines_list:
+                    if line.startswith('experiment'):
+                        print(line)
+                        expt_start2 = newlines_list.pop(newlines_list.index(line))
+                        expt_start2 = expt_start2.split(' ')
+                        start2_timecode = expt_start2[1]
+                        start2_hms = start2_timecode.split(':')
+                        start2_sec = (int(start2_hms[0])*(60**2) + int(start2_hms[1])*60 + int(start2_hms[2]))
+                    # else:
+                    #     print('Cannot find experiment start time')
+
+                start_diff = start2_sec - start1_sec
+
+                new_dict = {k:v for k,v in (val.split(':') for val in newlines_list)}
+
+                pass_time_new = float(new_dict['pass_time001']) + start_diff
+                new_pass = str(last_pass + 1)
+                new_str = '0' * (3-len(new_pass)) + new_pass
+
+                combo_dict['total_passes'] = new_pass
+                combo_dict['pass_valid'+new_str] = new_dict['pass_valid001']
+                combo_dict['pass_pos'+new_str] = new_dict['pass_pos001']
+                combo_dict['pass_time'+new_str] = str(pass_time_new)
+                combo_dict['experiment_start'] = (' ').join((start1_date, start1_timecode))
+
+                # print(combo_dict)
+                combo_list = [k+':'+v+'\n' for k,v in combo_dict.items()]
+                # if not os.path.exists('test'): os.makedirs('test')
+                with open(oldfile,'w') as file:
+                    for line in combo_list:
+                        file.write(line)
+            else:
+                new_str = '001'
+    return new_str
+#***********************************************************************************************#
+iris_path = input("\nPlease type in the path to the folder that contains the IRIS data:\n")
+if iris_path == 'test':
+    iris_path = "/Volumes/KatahdinHD/ResilioSync/DATA/IRIS/tCHIP_results/tXbCHIP025_VSV-EBOVmak@1E6-Dylight549"
+iris_path = iris_path.strip('"')##Point to the correct directory
+os.chdir(iris_path)
+
+if sys.platform == 'win32': splitter = ('\\')
+elif sys.platform == 'darwin': splitter = ('/')
+
+xml_list = [file for file in sorted(glob.glob('*/*.xml'))]
+if not xml_list: xml_list = [file for file in sorted(glob.glob('../*/*.xml'))]
+print("{} chipFiles detected".format(len(xml_list)))
+
+for xfile in xml_list:
+    chip_name = (xfile.split(splitter)[1]).split('.')[0]
+
+    print(chip_name)
+    folder_name = chip_name
+    # number = 1
+    all_files = sorted(glob.glob('{}.*'.format(chip_name)))
+    pgm_list, csv_list, png_list, txt_list, tiff_list = [],[],[],[],[]
+    for file in all_files:
+        if file.endswith('.pgm'):
+            pgm_list.append(file)
+        elif file.endswith('.txt'):
+            txt_list.append(file)
+        elif file.endswith('.csv'):
+            csv_list.append(file)
+        elif file.endswith('.png'):
+            png_list.append(file)
+        elif file.endswith('.tif'):
+            tiff_list.append(file)
+            print("TIFF images detected")
+
+    if pgm_list != []:
+        pgm_list = fluor_cleaner(pgm_list, splitter)
+        total_spots = max([int(val.split('.')[1]) for val in pgm_list])
+        nV_deleted(csv_list)
+        nV_deleted(png_list)
+
+    # if (tiff_list == []) & (pgm_list != []):
+    #     print("Converting TIFFs")
+    #     img_list = tiff_maker(pgm_list, archive = False)
+        if (len(tiff_list) != total_spots):
+            print("Converting TIFFs")
+            img_list = tiff_maker(pgm_list, archive = False)
+    else:
+        img_list = tiff_list
+
+    iris_data_list = img_list + txt_list
+    # print(iris_data_list)
+
+
+    if not iris_data_list == []:
+        if os.path.exists(folder_name):
+            os.chdir(folder_name)
+
+
+            try: new_str = txt_combiner(txt_list, splitter)
+            except UnboundLocalError: new_str = '002'
+
+            for txtfile in txt_list:
+                if len(txtfile.split('.')) < 4:
+                    os.remove('../' + txtfile)
+            os.chdir(iris_path)
+            mirror_file = str(glob.glob(folder_name+'.000.000.pgm')).strip("'[]'")
+            if mirror_file: #in iris_data_list:
+                iris_data_list.append(mirror_file)
+            #     os.remove(mirror_file)
+            print(iris_data_list)
+            for filename in iris_data_list:
+                filesplit = filename.split('.')
+                if filesplit[2].isdigit():
+                    filesplit[2] = new_str
+                    newname = '.'.join(filesplit)
+                    print(newname)
+                    dest = '{}{}{}'.format(folder_name, #splitter+'test'+
+                                           splitter, newname)
+                    os.rename(filename, dest)
+        else:
+            os.makedirs(folder_name)
+            for filename in iris_data_list:
+                dest = '{}{}{}'.format(folder_name, splitter, filename)
+                os.rename(filename, dest)
+
+
+
+
+
+
+    #     while os.path.exists(folder_name):
+    #         number += 1
+    #         folder_name = chip_name + '_{}'.format(number)
+
+    #
+    # # if not iris_data_list == []:
+    #     for file in iris_data_list:
+    #         dest = '{}/{}'.format(folder_name, file)
+    #         os.rename(file, dest)
+    #         print("Moving files...")
+    #
+    #     if folder_name != chip_name:
+    #         os.chdir(folder_name)
+    #         all_moved_files = sorted(glob.glob('*.*'))
+    #         iris_renamer(filelist = all_moved_files, number = number)
